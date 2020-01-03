a19d198fa0fc0a872bebf21ea7dd0b49484454a6
NebNooc <49407232+NebNooc@users.noreply.github.com>
2020-01-03 10:10:59 -0500

    Pal

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,38 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        // Finding all Palindromes in String
+        ArrayList<String> al = new ArrayList<>();
+        for(int i = 0; i < string.length(); i++) {
+            for(int j = i; j <= string.length(); j++) {
+                if(isPalindrome(string.substring(i, j)) && !al.contains(string.substring(i,j)) && !string.substring(i,j).equals(""))
+                    al.add(string.substring(i,j));
+            }
+        }
+        // Converting arraylist to array
+        String[] arr = new String[al.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = al.get(i);
+        }
+
+        return arr;
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        return string.equals(reverseString(string));
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for(int i = string.length() - 1; i >= 0; i--) {
+            sb.append(string.charAt(i));
+        }
+        return sb.toString();
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,11 +1,26 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
     public static String[] getAllSubstrings(String string) {
-        return null;
+        // Finding all Palindromes in String
+        ArrayList<String> al = new ArrayList<>();
+        for(int i = 0; i < string.length(); i++) {
+            for(int j = i; j <= string.length(); j++) {
+                al.add(string.substring(i,j));
+            }
+        }
+        // Converting arraylist to array
+        String[] arr = new String[al.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = al.get(i);
+        }
+
+        return arr;
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
